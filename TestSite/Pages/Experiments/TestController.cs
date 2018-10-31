8b4e5310a014778f3b2dbda8163d947e6708ac5c
Di1997 <damianignatiev@gmail.com>
2018-11-01 01:09:41 +0300

    Sync VS

---
+++ TestSite/Pages/Experiments/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestSite.Pages.Experiments
+{
+    public class TestController : Controller
+    {
+        public string Index()
+        {
+            return "This is just a test";
+        }
+
+        public string Test2()
+        {
+            return "This is test2!";
+        }
+    }
+}
